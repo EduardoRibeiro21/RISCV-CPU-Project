017f85d73348a890221881fb0c4f06fd273d9034
EduardoRibero21 <up202404396@up.pt>
2026-08-07 23:52:41 +0100

    Starting implementing extractions

--- cpu/decoder/decoder.py
+++ cpu/decoder/decoder.py
@@ -15,29 +15,29 @@ class Decoder:
         return instruction
 
     def extract_Itype(self, bits):
-            instruction = {
-                "opcode": self.get_opcode(bits),
-                "rd": (bits >> 7) & 0x1F,
-                "funct3": (bits >> 12) & 0x07,
-                "rs1": (bits >> 15) & 0x1F,
-                "imm": (bits >> 20) & 0xFFF
-            }
-    
-            return instruction
+        instruction = {
+            "opcode": self.get_opcode(bits),
+            "rd": (bits >> 7) & 0x1F,
+            "funct3": (bits >> 12) & 0x07,
+            "rs1": (bits >> 15) & 0x1F,
+            "imm": (bits >> 20) & 0xFFF
+        }
+
+        return instruction
 
     def extract_Stype(self, bits):
-            instruction = {
-                "opcode": self.get_opcode(bits),
-                "rd": (bits >> 7) & 0x1F,
-                "imm_1": (bits >> 12) & 0x07,
-                "rs1": (bits >> 15) & 0x1F,
-                "rs2": (bits >> 20) & 0x1F,
-                "imm_2": (bits >> 25) & 0x7F
-            }
+        instruction = {
+            "opcode": self.get_opcode(bits),
+            "rd": (bits >> 7) & 0x1F,
+            "imm_1": (bits >> 12) & 0x07,
+            "rs1": (bits >> 15) & 0x1F,
+            "rs2": (bits >> 20) & 0x1F,
+            "imm_2": (bits >> 25) & 0x7F
+        }
 
-            instruction["imm"] = (instruction["imm_1"] << 5) & instruction["imm_2"]
-    
-            return instruction
+        instruction["imm"] = (instruction["imm_1"] << 5) & instruction["imm_2"]
+
+        return instruction
 
     """TODO: implementing the others extractions"""
     def extract_Rtype(self, bits):
